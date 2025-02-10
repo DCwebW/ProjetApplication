dffd6c6c79c91e14a0de3242b7993f8124c416b9
DCwebW <149959426+DCwebW@users.noreply.github.com>
2025-02-11 00:13:12 +0100

    update suppression dossier android

--- ApplicationCourts/App.js
+++ ApplicationCourts/App.js
@@ -1,73 +1,73 @@
-import React, { useEffect, useState,  } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
-import AppLoading from 'expo-app-loading'; // Pour attendre que les polices soient chargées
+import * as SplashScreen from 'expo-splash-screen'; // Importation de SplashScreen
 import { useFonts } from 'expo-font';
 import Login from './components/screens/Login';
 import MotdepasseOublie from './components/screens/MotdepasseOublie';
 import Inscription from './components/screens/Inscription';
 import DrawerNavigator from './components/navigation/DrawerNavigator';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import PolitiqueRGPD from './components/screens/PolitiqueRGPD';
 import { FontProvider } from './components/ThemeContext/FontContext';
 
+// Empêche le splash screen de se cacher automatiquement
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 const auth = getAuth();
 
 export default function App() {
   const [user, setUser] = useState(null);
-  
-  
+  const [appReady, setAppReady] = useState(false);
+
   const [fontsLoaded] = useFonts({
     'Kanit-Light': require('./assets/fonts/Kanit-Light.ttf')
-  })
-  
+  });
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log('user', user);
       setUser(user);
     });
 
-    return () => {
-      // Nettoyer l'écouteur lorsque le composant est démonté
-      unsubscribe();
-    };
-  },[])
-  
-  // commentaire pour Github Actions   
+    return () => unsubscribe();
+  }, []);
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  useEffect(() => {
+    if (fontsLoaded) {
+      setAppReady(true);
+      SplashScreen.hideAsync(); // Cache le splash screen une fois les polices chargées
+    }
+  }, [fontsLoaded]);
+
+  if (!appReady) {
+    return null; // Affiche un écran vide jusqu'à ce que tout soit prêt
   }
 
   return (
     <FontProvider>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? 'Home' : 'Login'} 
-        screenOptions={{
-          headerStyle: { backgroundColor: 'rgba(197, 44, 35,1)' },
-          headerTintColor: 'white'
-        }}
-      >
-        {user !== null ? (
-          
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={user ? 'Home' : 'Login'}
+          screenOptions={{
+            headerStyle: { backgroundColor: 'rgba(197, 44, 35,1)' },
+            headerTintColor: 'white',
+          }}
+        >
+          {user !== null ? (
             <Stack.Screen name='Home' component={DrawerNavigator} options={{ headerShown: false }}/>
-          
-        ) : (
-          <>
-            <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-            <Stack.Screen name='Reinitialisation Mot de passe' component={MotdepasseOublie}  />
-            <Stack.Screen name='Inscription' component={Inscription} />
-            <Stack.Screen name='Politique de Confidentialité' component={PolitiqueRGPD}/> 
-          </>
-        )}
-      </Stack.Navigator>
-
-      {/* Ajout du composant Connexionauto */}
-      {/* <Connexionauto /> */}
-    </NavigationContainer>
+          ) : (
+            <>
+              <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
+              <Stack.Screen name='Reinitialisation Mot de passe' component={MotdepasseOublie} />
+              <Stack.Screen name='Inscription' component={Inscription} />
+              <Stack.Screen name='Politique de Confidentialité' component={PolitiqueRGPD} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </FontProvider>
   );
 }
